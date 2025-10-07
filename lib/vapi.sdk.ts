94938036e95ccf2a1b6812852e58a76f024c614c
mominrkhan <vadehqs@gmail.com>
2025-10-07 19:44:23 -0400

    debug: add logging to identify VAPI integration issues

--- lib/vapi.sdk.ts
+++ lib/vapi.sdk.ts
@@ -1,3 +1,9 @@
 import Vapi from "@vapi-ai/web";
 
-export const vapi = new Vapi(process.env.NEXT_PUBLIC_VAPI_WEB_TOKEN!);
+const vapiToken = process.env.NEXT_PUBLIC_VAPI_WEB_TOKEN;
+
+if (!vapiToken) {
+  console.error("NEXT_PUBLIC_VAPI_WEB_TOKEN is not defined");
+}
+
+export const vapi = new Vapi(vapiToken!);
